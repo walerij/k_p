94d48b07c670a11e90f93c9c1a18e453a8b031e9
Valeron <becwal@yandex.ru>
2024-06-18 10:02:13 +0300

    класс работы с программой

--- img_.py
+++ img_.py
@@ -17,3 +17,27 @@ class ImageManage():
     def save (self, image, filename):
         img = self.open(image)
         img.save(filename)
+
+
+  #получение параметров
+    def get_params(self, image):
+
+        img = self.open(image)
+        tmp= {'widht':img.width,'height':img.height,'filename':img.filename,'format':img.format,'mode':img.mode}
+        return tmp
+
+    def rotate(self, image, angle):
+        img = self.open(image)
+        img=img.rotate(angle)
+        return img
+
+    #наложение
+    def paste(self, image1, image2, x1=0,y1=0,x2=20, y2=20):
+        img = Image.open(image1)
+        img2 = Image.open(image2)
+
+        img=img.paste(img2, (x1,y1,x2,y2))
+        return img
+
+
+    #зеркальный поворот
